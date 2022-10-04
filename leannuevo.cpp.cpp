9a7a2f4864841dceb7756bbb68bd25a48e5ae09b
Leandro Oporta <leandrooporta031@gmail.com>
2022-10-04 09:49:21 -0600

    se agrego el archivo leannuevo.cpp

---
+++ leannuevo.cpp.cpp
@@ -0,0 +1,15 @@
+
+/*
+Nombre del archivo: leannuevo.cpp
+Autor: Lean
+Lugar: ITV
+//Instrucciones: Esto solo es una platilla
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
