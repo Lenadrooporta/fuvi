b9d2f5f3c70495df639e15106f6152b1cba15bf7
Leandro Oporta <leandrooporta031@gmail.com>
2022-10-04 10:14:36 -0600

    se modifico el archivo leannuevo y pitagoras es nuevo

--- leannuevo.cpp.cpp
+++ leannuevo.cpp.cpp
@@ -6,10 +6,20 @@ Lugar: ITV
 //Instrucciones: Esto solo es una platilla
 */
 #include<iostream>
+#include<cmath>
 
 using namespace std;
+//variables globales
+const double pi = 3.141592;
 
 int main(){
 	
+	double radio, area;
+	cout << "Ingrese el rado del circulo: ";
+	cin >> radio;
+	area = pi * pow(radio,2);
+	cout << "El ara de un circulo con radio" << radio << " es " << area ;
+	
+ 	
 	return 0;
 }
